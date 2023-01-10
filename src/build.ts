dfd905ed53d8f0ffe06127823d6a67ca21ca1fe3
Aaron Sutula <asutula@users.noreply.github.com>
2023-01-10 16:13:38 -0600

    hard code validator version
    Signed-off-by: Aaron Sutula <asutula@users.noreply.github.com>

--- src/build.ts
+++ src/build.ts
@@ -7,9 +7,9 @@ import decompress from "decompress";
 import shelljs from "shelljs";
 import tar from "tar";
 // src
-import { getDirname, getVersion } from "./module-specific.js";
+import { getDirname } from "./module-specific.js";
 
-const version = getVersion();
+const version = "0.0.1-alpha-3";
 const _dirname = getDirname();
 
 const releaseRepoUrl = "https://github.com/tablelandnetwork/go-tableland";
--- src/module-specific-cjs.ts
+++ src/module-specific-cjs.ts
@@ -4,15 +4,3 @@
 export const getDirname = function (): string {
   return __dirname;
 };
-
-export const getVersion = function (): string {
-  // eslint-disable-next-line @typescript-eslint/no-var-requires
-  let version = require("../../package.json").version;
-
-  // strip a leading v off the version so that we normalize v0.1.0 and 0.1.0 to be the same value
-  if (/^v[0-9]/.test(version)) {
-    version = version.slice(1);
-  }
-
-  return version;
-};
